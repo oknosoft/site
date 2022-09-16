837cf64b9d044781f549e27034b1b4ddce79f5e6
unpete <info@oknosoft.ru>
2022-09-17 03:15:16 +0500

    sitemap

--- server/articles/index.js
+++ server/articles/index.js
@@ -3,110 +3,21 @@
  * для смешанного рендеринга
  */
 
-const https = require('https');
-const marked = require('marked');
-const sitemap = require('./sitemap');
-const {resolve} = require('path');
-const {appBuild} = require('../../config/paths');
-const {readFile} = require('node:fs/promises');
 
-const cache = {
-  title: 'Окнософт',
-  description: require('./description'),
-  cprefix: '/couchdb/www_0_ram/cat.articles|',
-  async load(file = 'index') {
-    const html = this[file];
-    if(html) {
-      return html;
-    }
-    try {
-      this[file] = await readFile(resolve(appBuild, `${file}.html`), 'utf8');
-    }
-    catch (e) {}
-    return this[file];
-  },
-};
+const sitemap = require('./sitemap');
+const article = require('./article');
 
 module.exports = function ({cat: {articles}}, log) {
 
   // вернём handler
   return async (req, res) => {
-    let {query, path, paths} = req.parsed;
+    let {path} = req.parsed;
     path = path.replace(/^\//, '');
 
-    if(!path || path.startsWith('index')) {
-      res.setHeader('Content-Type', 'text/html; charset=utf-8');
-      res.end(await fill());
-      return true;
+    if(path.startsWith('sitemap.xml')) {
+      return sitemap(req, res, articles, log);
     }
 
-    const slash = /(\.html|\/)$/.test(path);
-    if(slash) {
-      path = path.replace(/(\.html|\/)$/, '');
-    }
-    const article = articles._aliases[path];
-    if(article) {
-      // если статья найдена, но кривой путь
-      if(path !== article.id || slash || query) {
-        res.writeHead(301, {location: `/${article.id}`});
-        res.end();
-      }
-      // если статья найдена и запросили сырые данные
-      else if(req.method === 'POST' || req.headers.accept === 'application/json') {
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(article));
-      }
-      else {
-        // если статья найдена и запросили html
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.end(await fill(article));
-      }
-    }
-    else {
-      // если статья не найдена
-      res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
-      res.end(await fill('404'));
-    }
-    return true;
-  }
-}
-
-async function fill(doc) {
-  let html = await cache.load();
-  if(typeof doc === 'string') {
-    html = html.replace(`<div id="root"></div>`, await cache.load(doc));
-  }
-  else if(!doc) {
-    html = html.replace(`<div id="root"></div>`, await cache.load('home'));
-  }
-  else {
-    const title = doc.name;
-    const descr = doc.descr || doc.introduction;
-    html = html.replace(
-      `<title>${cache.title}</title>`,
-      `<title>${title}</title>`);
-    html = html.replace(
-      `<meta property="og:title" content="${cache.title}" data-react-helmet="true">`,
-      `<meta property="og:title" content="${title}" data-react-helmet="true">`);
-    html = html.replace(
-      `<meta name="description" content="${cache.description}" data-react-helmet="true">`,
-      `<meta name="description" content="${descr}" data-react-helmet="true">`);
-    html = html.replace(
-      `<meta property="og:description" content="${cache.description}" data-react-helmet="true">`,
-      `<meta property="og:description" content="${descr}" data-react-helmet="true">`);
-    html = html.replace(
-      `<div id="root"></div>`,
-      `<div id="root"><div style="max-width: 960px; padding: 48px 8px; margin: auto; color: #777;">
-<h1>${doc.h1 || doc.name}</h1>${marked.parse(
-        (doc.content || '')
-          .replace(/\!\[image\]\(this/gm, `![image](${cache.cprefix}${doc.ref}`)
-          .replace(/src="this\//gm, `src="${cache.cprefix}${doc.ref}/`),
-      )}</div></div>`);
-
-    if(doc.img) {
-      html = html.replace('https://oknosoft.ru/imgs/flask_192.png', doc.img);
-    }
-  }
-  return html;
-}
-
+    return article(req, res, articles, log);
+  };
+};
--- server/articles/sitemap.js
+++ server/articles/sitemap.js
@@ -7,61 +7,24 @@
  */
 
 const xml = require('fs').readFileSync(require.resolve('./sitemap.xml'), 'utf8');
-const ram = [];
-const selector = {
-  selector: {
-    class_name: 'cat.articles',
-    date: {$gt: null},
-    search: {$gt: null},
-    published: true
-  },
-  fields: ['id', 'date', 'tags', 'acl'],
-  limit: 1000,
-};
 const su = process.env.SERVER_URL;
 
-function row({id, date, tags}) {
-  // находим tag
-  const tag = tags && tags.length && ram.find((row) => {
-    return row._id.indexOf(tags[0]) !== -1;
-  });
-  // находим group
-  let category = tag && ram.find((row) => {
-    return row._id.indexOf(tag.category) !== -1;
-  });
-  if(!category && /terms_of_use|decent_subscription|privacy_policy/.test(id)) {
-    category = {predefined_name: 'articles'};
-  }
-  let cat = category && category.predefined_name;
-  if(!cat) {
-    console.error(id, date, tags);
-    return '';
-  }
-  if(!cat.endsWith('s')) {
-    cat += 's';
-  }
-  return category ? `<url>
-    <loc>${su}/${cat}/${id}</loc>
-    <lastmod>${date.split('T')[0]}</lastmod>
-  </url>` : '';
+function row({id, date}) {
+  return `<url>
+    <loc>${su}/${id}</loc>
+    <lastmod>${date.toISOString().split('T')[0]}</lastmod>
+</url>`;
 }
 
-module.exports = function (req, res, next, db) {
-  if(!req.url.match(/^\/sitemap\.xml/)) {
-    return next();
-  }
-
+module.exports = function (req, res, articles, log) {
+  let rows = '';
   // бежим по всем статьям
-  db.find(selector)
-    .then(({docs}) => {
-      let rows = '';
-      for(const doc of docs) {
-        if(doc.acl && doc.acl.includes('_anonymous')){
-          rows += row(doc);
-        }
-      }
-      res
-        .set('Content-Type', 'application/xml; charset=utf-8')
-        .send(xml.replace('</urlset>', `${rows}\n</urlset>`));
-    });
-}
+  for(const doc of articles) {
+    if(doc.published){
+      rows += row(doc);
+    }
+  }
+  res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+  res.end(xml.replace('</urlset>', `${rows}\n</urlset>`));
+  return true;
+};
